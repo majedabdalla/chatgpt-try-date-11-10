a11c5a73b7f180ba3e5091824e34560a43e3f2a3
majedabdalla <125223672+majedabdalla@users.noreply.github.com>
2025-10-23 03:45:06 +0300

    Update db.py

--- db.py
+++ db.py
@@ -20,8 +20,23 @@ db = client["anonindochat"]
 
 async def get_user(user_id):
     user = await db.users.find_one({"user_id": user_id})
+    # Always provide all expected attributes to default_user
+    username = ""
+    phone_number = ""
+    full_name = ""
+    first_name = ""
     if user:
-        defaults = default_user(type('TelegramUser', (), {'id': user_id, 'username': user.get('username', ''), 'phone_number': user.get('phone_number', ''), 'full_name': user.get('name',''), 'first_name': user.get('name','')})())
+        username = user.get('username', '')
+        phone_number = user.get('phone_number', '')
+        full_name = user.get('name', '')
+        first_name = user.get('first_name', '')
+        defaults = default_user(type('TelegramUser', (), {
+            'id': user_id,
+            'username': username,
+            'phone_number': phone_number,
+            'full_name': full_name,
+            'first_name': first_name
+        })())
         for k, v in defaults.items():
             if k not in user:
                 user[k] = v
@@ -29,8 +44,19 @@ async def get_user(user_id):
 
 async def get_user_by_username(username):
     user = await db.users.find_one({"username": username})
+    # Always provide all expected attributes to default_user
+    user_id = user['user_id'] if user else 0
+    phone_number = user.get('phone_number', '') if user else ''
+    full_name = user.get('name', '') if user else ''
+    first_name = user.get('first_name', '') if user else ''
+    defaults = default_user(type('TelegramUser', (), {
+        'id': user_id,
+        'username': username,
+        'phone_number': phone_number,
+        'full_name': full_name,
+        'first_name': first_name
+    })())
     if user:
-        defaults = default_user(type('TelegramUser', (), {'id': user['user_id'], 'username': user.get('username', ''), 'phone_number': user.get('phone_number', ''), 'full_name': user.get('name',''), 'first_name': user.get('name','')})())
         for k, v in defaults.items():
             if k not in user:
                 user[k] = v
@@ -38,47 +64,31 @@ async def get_user_by_username(username):
 
 async def update_user(user_id, updates):
     doc = await db.users.find_one({"user_id": user_id})
+    username = updates.get('username', '')
+    phone_number = updates.get('phone_number', '')
+    full_name = updates.get('name', '')
+    first_name = updates.get('first_name', '')
     if not doc:
-        temp_user = type('TelegramUser', (), {'id': user_id, **updates})()
+        temp_user = type('TelegramUser', (), {
+            'id': user_id,
+            'username': username,
+            'phone_number': phone_number,
+            'full_name': full_name,
+            'first_name': first_name
+        })()
         full_doc = default_user(temp_user)
         full_doc.update(updates)
         await db.users.update_one({"user_id": user_id}, {"$set": full_doc}, upsert=True)
     else:
         doc.update(updates)
-        defaults = default_user(type('TelegramUser', (), {'id': user_id, 'username': doc.get('username', ''), 'phone_number': doc.get('phone_number', ''), 'full_name': doc.get('name',''), 'first_name': doc.get('name','')})())
+        defaults = default_user(type('TelegramUser', (), {
+            'id': user_id,
+            'username': doc.get('username', ''),
+            'phone_number': doc.get('phone_number', ''),
+            'full_name': doc.get('name', ''),
+            'first_name': doc.get('first_name', '')
+        })())
         for k, v in defaults.items():
             if k not in doc:
                 doc[k] = v
         await db.users.update_one({"user_id": user_id}, {"$set": doc}, upsert=True)
-
-async def get_room(room_id):
-    return await db.rooms.find_one({"room_id": room_id})
-
-async def update_room(room_id, updates):
-    await db.rooms.update_one({"room_id": room_id}, {"$set": updates})
-
-async def insert_room(room_data):
-    await db.rooms.insert_one(room_data)
-
-async def delete_room(room_id):
-    await db.rooms.delete_one({"room_id": room_id})
-
-async def insert_report(report_data):
-    await db.reports.insert_one(report_data)
-
-async def insert_blocked_word(word):
-    await db.blocked_words.update_one({"word": word}, {"$set": {"word": word}}, upsert=True)
-
-async def remove_blocked_word(word):
-    await db.blocked_words.delete_one({"word": word})
-
-async def get_blocked_words():
-    cursor = db.blocked_words.find({})
-    return [doc["word"] async for doc in cursor]
-
-async def log_chat(room_id, log_data):
-    await db.chatlogs.insert_one({"room_id": room_id, **log_data})
-
-async def get_chat_history(room_id):
-    cursor = db.chatlogs.find({"room_id": room_id})
-    return [doc async for doc in cursor]
